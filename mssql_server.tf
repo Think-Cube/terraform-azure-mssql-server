0007ecad0617ad9dd060f5812737782ec7aae504
spy86 <contact@mmichalski.com>
2024-04-09 21:31:29 +0200

    1. Update module files 2. Create examples

--- mssql_server.tf
+++ mssql_server.tf
@@ -3,11 +3,11 @@ resource "azurerm_mssql_server" "main" {
   resource_group_name           = data.azurerm_resource_group.main.name
   location                      = data.azurerm_resource_group.main.location
   version                       = var.mssql_server_version
-  administrator_login           = var.mssql_serveradmin_login
-  administrator_login_password  = var.mssql_serveradmin_password
-  minimum_tls_version           = var.mssql_serverminimum_tls_version
-  public_network_access_enabled = var.mssql_serverpublic_network_access_enabled
-  connection_policy             = var.mssql_serverconnection_policy
+  administrator_login           = var.mssql_server_admin_login
+  administrator_login_password  = var.mssql_server_admin_password
+  minimum_tls_version           = var.mssql_server_minimum_tls_version
+  public_network_access_enabled = var.mssql_server_public_network_access_enabled
+  connection_policy             = var.mssql_server_connection_policy
   tags                          = var.default_tags
 
   lifecycle {
@@ -22,8 +22,8 @@ resource "azurerm_mssql_server" "main" {
   }
 
   azuread_administrator {
-    login_username = var.mssql_serverazure_ad_admin_login
-    object_id      = var.mssql_serverazure_ad_admin_object_id
-    tenant_id      = var.azure_tenant_id
+    login_username = var.mssql_server_azure_ad_admin_login
+    object_id      = var.mssql_server_azure_ad_admin_object_id
+    tenant_id      = var.mssql_server_azure_tenant_id
   }
 }
--- variables.tf
+++ variables.tf
@@ -42,42 +42,42 @@ variable "mssql_server_version" {
   type        = string
   default     = 12.0
 }
-variable "mssql_serveradmin_login" {
+variable "mssql_server_admin_login" {
   description = "The administrator login name for the new server. Required unless azuread_authentication_only in the azuread_administrator block is true. When omitted, Azure will generate a default username which cannot be subsequently changed. Changing this forces a new resource to be created."
   type        = string
 }
-variable "mssql_serveradmin_password" {
+variable "mssql_server_admin_password" {
   description = "The password associated with the administrator_login user. Needs to comply with Azure's Password Policy. Required unless azuread_authentication_only in the azuread_administrator block is true."
   type        = string
 }
-variable "mssql_serverminimum_tls_version" {
+variable "mssql_server_minimum_tls_version" {
   description = "The Minimum TLS Version for all SQL Database and SQL Data Warehouse databases associated with the server. Valid values are: 1.0, 1.1 , 1.2 and Disabled. Defaults to 1.2."
   type        = string
   default     = "1.2"
 }
-variable "mssql_serverpublic_network_access_enabled" {
+variable "mssql_server_public_network_access_enabled" {
   description = "Whether public network access is allowed for this server. Defaults to true."
   type        = bool
   default     = true
 }
-variable "mssql_serverconnection_policy" {
+variable "mssql_server_connection_policy" {
   description = "The connection policy the server will use. Possible values are Default, Proxy, and Redirect. Defaults to Default."
   type        = string
   default     = "Default"
 }
-variable "mssql_serverazure_ad_admin_login" {
+variable "mssql_server_azure_ad_admin_login" {
   description = "The login username of the Azure AD Administrator of this SQL Server."
   type        = string
 }
-variable "mssql_serverazure_ad_admin_object_id" {
+variable "mssql_server_azure_ad_admin_object_id" {
   description = "The object id of the Azure AD Administrator of this SQL Server."
   type        = string
 }
-variable "azure_tenant_id" {
+variable "mssql_server_azure_tenant_id" {
   description = "The tenant id of the Azure AD Administrator of this SQL Server."
   type        = string
 }
-variable "ip_rules" {
+variable "mssql_server_ip_rules" {
   type        = map(string)
   description = "Map of IP addresses permitted for access to DB"
   default     = {}
